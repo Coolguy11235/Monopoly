6d7c8062df4e5b859bfa8e034409f6b9c1af8734
tim.safronov <tim.safronov@elev.ga.ntig.se>
2024-04-27 18:58:43 +0200

    Har lagt till metoder för Dice och Player, därefter började med Square

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,37 @@
+import java.util.HashMap;
+
+// klassen föreställer alla spelarna
 public class Player {
+
+    // Privata fälter:
+    private int playerNumber;
+    static int totalPlayers = 0;
+    static HashMap<Integer, Integer> ledger = new HashMap<>();
+
+    private int wallet = 1500; // Antalet pengar man börjar med
+
+    // Public metoder:
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public int getWallet() {
+        return wallet;
+    }
+
+    public void withdrawFromWallet(int withdrawAmount) {
+        if (withdrawAmount > wallet) {
+            //setVisible(false);
+            System.out.println("Spelaren " + playerNumber + " gick i konkurs!");
+        } else {
+            wallet -= withdrawAmount;
+        }
+    }
+
+    public void depositToWallet(int depositAmount) {
+        wallet += depositAmount;
+        System.out.println("Avlöningsdag för spelaren" + playerNumber + ". Du fick ###$!");
+    }
+
 
 }
--- src/Square.java
+++ src/Square.java
@@ -1,2 +1,13 @@
 public class Square {
+
+    // Privata fälter:
+    int number;
+    private String name;
+    String description;
+    static int totalSquares = 0;
+    private int price;
+    private int rentPrice;
+
+    // Public metoder:
+
 }
