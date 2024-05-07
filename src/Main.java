d8ff17a7749b25797acc9085a22caa3769bca31d
tim.safronov <tim.safronov@elev.ga.ntig.se>
2024-05-07 12:44:45 +0200

    Har lagt till en kommentar

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Startar spelet genom att implementera Game-klassen
         Game frame = new Game();
         frame.setVisible(true);
 
